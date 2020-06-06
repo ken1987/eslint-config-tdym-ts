a804f69b128e774420cb167b660fae4a5cb8428f
ken1987 <ken198701@gmail.com>
2020-06-06 17:54:55 +0800

    feat: 更新规则

--- index.js
+++ index.js
@@ -2,6 +2,12 @@ module.exports = {
   env: {
     node: true,
   },
+  settings: {
+    // https://github.com/benmosher/eslint-plugin-import/issues/1396#issuecomment-511007063
+    'import/resolver': {
+      node: {},
+    },
+  },
   extends: [
     './configs/vue.js',
     './configs/airbnb.js',
